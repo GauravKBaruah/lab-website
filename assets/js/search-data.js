f3825be7795cc805b799338ea6937926d072f464
GauravKBaruah <GauravKBaruah@users.noreply.github.com>
2026-03-20 17:43:53 +0000

    Deploying to gh-pages from @ GauravKBaruah/lab-website@2401bbf1aca39b18eadbeca171d804f53f6f31fd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -479,13 +479,6 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/lab-website/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/lab-website/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
@@ -493,11 +486,32 @@ ninja.data = [{
           window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/gauravkbaruah", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/https://www.linkedin.com/in/gaurav-baruah-77845439a/", "_blank");
+        },
+      },{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://bsky.app/profile/gauravbaruah.bsky.social", "_blank");
+        },
+      },{
+        id: 'social-work',
+        title: 'Work',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://ces.iisc.ac.in/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -511,14 +525,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=NUBQBAsAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
