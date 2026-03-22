0b350bc5965f5d4d49e6754fac91b494ced51817
GauravKBaruah <GauravKBaruah@users.noreply.github.com>
2026-03-22 14:42:01 +0000

    Deploying to gh-pages from @ GauravKBaruah/lab-website@013f15d913ad5f4fb8dd6933ccaecf36b6a9eb63 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -220,17 +220,6 @@ ninja.data = [{
             window.location.href = "/lab-website/blog/2023/videos/";
           
         },
-      },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
-        
-          title: "displaying beautiful tables with Bootstrap Tables",
-        
-        description: "an example of how to use Bootstrap Tables",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/lab-website/blog/2023/tables/";
-          
-        },
       },{id: "post-a-post-with-table-of-contents",
         
           title: "a post with table of contents",
