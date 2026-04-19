9293e199bca77006d7d75ac96ca0580c2af849e3
GauravKBaruah <GauravKBaruah@users.noreply.github.com>
2026-04-19 08:21:59 +0000

    Deploying to gh-pages from @ GauravKBaruah/lab-website@2263ea341026ab4525cc5901faf61c07dd2ada6d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -121,17 +121,6 @@ ninja.data = [{
             window.location.href = "/lab-website/blog/2021/diagrams/";
           
         },
-      },{id: "post-a-post-with-disqus-comments",
-        
-          title: "a post with disqus comments",
-        
-        description: "an example of a blog post with disqus comments",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/lab-website/blog/2015/disqus-comments/";
-          
-        },
       },{id: "post-a-post-with-math",
         
           title: "a post with math",
