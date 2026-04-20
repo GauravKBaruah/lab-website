c600466b30ed4861c3492165b9c265314fc5337d
GauravKBaruah <GauravKBaruah@users.noreply.github.com>
2026-04-20 07:26:15 +0000

    Deploying to gh-pages from @ GauravKBaruah/gauravkbaruah.github.io@15ebabe0146a9b70e17145e86a8bc5e79f936080 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "GauravKBaruah/lab-website",
+    "data-repo": "GauravKBaruah/gauravkbaruah.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
